353a84e890410e826c373664b0352698d0a1a5c5
jlncrml <creepybluehair@gmail.com>
2025-06-24 20:58:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def index():
 @app.route('/transcript')
 def transcript():
     video_identifier = request.args.get("video_id")
+    language = request.args.get("language", default="sv")
     if not video_identifier:
         return jsonify({"error": "missing video_id"}), 400
     try:
-        transcript_data = YouTubeTranscriptApi.get_transcript(video_identifier)
+        transcript_data = YouTubeTranscriptApi.get_transcript(video_identifier, languages=[language])
         return jsonify(transcript_data)
     except Exception as exception:
         return jsonify({"error": str(exception)}), 500
